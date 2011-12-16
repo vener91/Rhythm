8ec319edde0cfc31a78cfb22bce76431ed376204
Reizo <vener88@gmail.com>
2011-12-16 17:13:43 -0600

    Change facebook stuff to match rhythm.no.de

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -5,9 +5,9 @@
 module.exports = function(app){
   app.get('/auth/facebook', app.passport.authenticate('facebook'), function(req, res){} );
 
-  app.get('/auth/facebook/callback', app.passport.authenticate('facebook', { failureRedirect: '/login' }),
+  app.get('/auth/facebook/callback', app.passport.authenticate('facebook', { failureRedirect: '/' }),
     function(req, res) {
       // Successful authentication, redirect home.
-      res.redirect('/');
+      res.redirect('/dash');
     });
 };
