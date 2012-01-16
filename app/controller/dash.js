58cc8c626172545950dbe56be6a19c68d46fd851
Reizo <vener88@gmail.com>
2012-01-16 11:18:42 -0600

    FB picture integration + username

--- app/controller/dash.js
+++ app/controller/dash.js
@@ -1,5 +1,12 @@
 module.exports = function(app){
     app.get('/dash', app.passport.isLoggedIn, function(req, res){
+
+    	//Get profile pic
+    	if(typeof(user.facebook_id) != 'undefined'){
+    		req.user.pic = "https://graph.facebook.com/" + user.facebook_id + "/picture";
+    	} else {
+    		req.user.pic = "/res/img/system/user.png";
+    	}
         res.render('dash', {
 	        layout: 'page-layout',
             title: app.title + '- Dashboard',
